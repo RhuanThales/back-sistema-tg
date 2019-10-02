c9dcdc9f6a589efeb2d4350a566720b79be3f84d
m4sterin <42187123+m4sterin@users.noreply.github.com>
2019-10-02 16:59:34 -0300

    Adiciando as classes na AutoMapper.
    Fiz o modificação pelo bloco de notas.

--- AutoMapperProfileConfiguration.cs
+++ AutoMapperProfileConfiguration.cs
@@ -13,6 +13,32 @@ namespace back_sistema_tg
                 AfterMap((dto, model) => model.Id = dto.Id);
             CreateMap<Usuario, UsuarioDTO>()
                 .AfterMap((model, dto) => dto.Id = model.Id);
+
+            // Mapeando a classe Atirador
+            CreateMap<AtiradorDTO, Atirador>().
+                AfterMap((dto, model) => model.IdAtirador = dto.IdAtirador);
+            CreateMap<Atirador, UsuarioDTO>()
+                .AfterMap((model, dto) => dto.IdAtirador = model.IdAtirador);
+
+                // Mapeando a classe Oficial
+            CreateMap<OficialDTO, Oficial>().
+                AfterMap((dto, model) => model.IdOficial = dto.IdOficial);
+            CreateMap<Oficial, OficialDTO>()
+                .AfterMap((model, dto) => dto.IdOficial = model.IdOficial);
+
+                // Mapeando a classe Pelotao
+            CreateMap<PelotaoDTO, Pelotao>().
+                AfterMap((dto, model) => model.IdPelotao = dto.IdPelotao);
+            CreateMap<Pelotao, PelotaoDTO>()
+                .AfterMap((model, dto) => dto.IdPelotao = model.IdPelotao);
+
+                // Mapeando a classe Escala
+            CreateMap<EscalaDTO, Escala>().
+                AfterMap((dto, model) => model.IdEscala = dto.IdEscala);
+            CreateMap<Escala, EscalaDTO>()
+                .AfterMap((model, dto) => dto.IdEscala = model.IdEscala);
+
+               
         }
     }
 }
