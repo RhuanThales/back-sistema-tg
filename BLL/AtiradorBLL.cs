e4d96b6ef9b3bf14f2ae83053bf0cac8983e3990
Caio <caio_rodrigues19@hotmail.com>
2019-10-04 17:42:49 -0300

    Criacao da BLL do atirador

---
+++ BLL/AtiradorBLL.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using back_sistema_tg.BLL.Exceptions;
+using back_sistema_tg.DAL.DAO;
+using back_sistema_tg.DAL.DTO;
+using back_sistema_tg.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace back_sistema_tg.BLL
+{
+    public class AtiradorBll : IAtiradorBll
+    {
+        public readonly IAtiradorDAO _atiradorDAO;
+
+        public AtiradorBll(IAtiradorDAO atiradorDAO)
+        {
+            _atiradorDAO = atiradorDAO;
+        }
+
+        public void Inserir(Atirador atirador)
+        {
+            _atiradorDAO.Inserir(atirador);
+        }
+
+        public List<Atirador> ObterTodos()
+        {
+            var listaAtirador = _atiradorDAO.ObterTodos();
+
+            return listaAtirador;
+        }
+
+        public Atirador ObterPorId(string IdAtirador)
+        {
+            var atirador = _atiradorDAO.ObterPorId(IdAtirador);
+
+            return atirador;
+        }
+
+        public void Atualizar(string IdAtirador, Atirador novoAtirador)
+        {
+            bool hasAny = (_atiradorDAO.ObterPorId(IdAtirador))!=null;
+
+            if (!hasAny)
+            {
+                throw new NotFoundException("Id não encontrado.");
+            }
+                
+            try
+            {
+                _atiradorDAO.Atualizar(IdAtirador, novoAtirador);
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        } 
+        
+        public void Excluir(string IdAtirador)
+        {           
+            var obj = _atiradorDAO.ObterPorId(IdAtirador);
+
+            bool hasAny = obj!=null;
+            
+            if (!hasAny)
+            {
+                throw new NotFoundException("Id não encontrado.");
+            }
+
+            try
+            {
+                _atiradorDAO.Excluir(obj.IdAtirador);
+            }
+            catch (DbUpdateException)
+            {
+                throw new IntegrityException("Não foi possível efetuar a remoção.");
+            }
+        }
+    }
+}
---
+++ BLL/IAtiradorBLL.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using back_sistema_tg.DAL.DTO;
+using back_sistema_tg.DAL.Models;
+
+namespace back_sistema_tg.BLL
+{
+    public interface IAtiradorBll
+    {
+        // Create
+        void Inserir(Atirador atirador);
+        // Read
+        List<Atirador> ObterTodos();
+        Atirador ObterPorId(string IdAtirador);
+        // Update
+        void Atualizar(string id, Atirador novoAtirador);
+        // Delete
+        void Excluir(string id);
+    }
+}
