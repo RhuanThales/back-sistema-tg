7fe347d2a267451cb6bd354f889a75b8963d083d
Rhuan Thales <rhuantst@gmail.com>
2019-11-04 17:27:14 -0200

    Adicionando atributo tipoEscala em Escalas

--- DAL/Models/Diaria.cs
+++ DAL/Models/Diaria.cs
@@ -4,10 +4,11 @@ namespace back_sistema_tg.DAL.Models
 {
     public class Diaria
     {
+        public string TipoEscala { get; set; }
+        public DateTime Dia { get; set; }
         public string [] PermanenciaManha { get; set; }
         public string [] PermanenciaTarde { get; set; }
         public string ComandanteGuarda { get; set; }
         public string [] Guardas { get; set; }
-        public DateTime Dia { get; set; }
     }
 }
